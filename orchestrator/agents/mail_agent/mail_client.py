e9dc7de106f4c4f794e16b7a35f9737cd92239bb
owen-taylor <89555814+owenT8@users.noreply.github.com>
2026-06-13 12:34:12 -0600

    test commit

--- orchestrator/agents/mail_agent/mail_client.py
+++ orchestrator/agents/mail_agent/mail_client.py
@@ -7,21 +7,27 @@ load_dotenv()
 
 @dataclass
 class Email:
+    uid: str
     sender: str
     subject: str
     body: str
 
 class MailClient:
     def __init__(self):
-        user = os.getenv("GOOGLE_USER")
-        password = os.getenv("GOOGLE_PASSWORD")
-        self.mail_box = MailBox(host="imap.gmail.com", port=993).login(user, password)
+        self.user = os.getenv("GOOGLE_USER")
+        self.password = os.getenv("GOOGLE_PASSWORD")
+        self.mail_box = MailBox(host="imap.gmail.com", port=993).login(self.user, self.password)
 
     def getUnreadEmails(self) -> list:
+        # Reset the mail box to ensure we get the latest emails
+        self.mail_box = MailBox(host="imap.gmail.com", port=993).login(self.user, self.password)
         messages = self.mail_box.fetch(criteria=AND(seen=False))
         processed_emails = []
 
         for message in messages:
-            processed_emails.append(Email(message.from_, message.subject, message.text))
+            processed_emails.append(Email(message.uid, message.from_, message.subject, message.text))
 
         return processed_emails
+
+    def moveToFolder(self, email_uid: str, folder: str):
+        self.mail_box.move(email_uid, folder)
